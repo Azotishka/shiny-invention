717d3d1b77c96117279bf08615e8dc0f2f83a0f3
Azotishka <vaa2133339@yandex.ru>
2026-09-23 00:46:57 +0500

    Set conservative Watchy V2 defaults

---
+++ firmware/NeuroWatch_OS/neuro_config.h
@@ -0,0 +1,9 @@
+#pragma once
+// Do not store any Wi-Fi credentials or personal tokens in this header.
+#define NW_FACE 0
+#define NW_SHOW_VOLTAGE 1
+#define NW_SHOW_HEAP 1
+#define NW_SHOW_WIFI_SAVED 1
+#define NW_UTC_OFFSET_SECONDS (5 * 3600)
+#define NW_MIN_OTA_VOLTAGE 3.75f
+#define NW_OTA_TIMEOUT_SECONDS 180
